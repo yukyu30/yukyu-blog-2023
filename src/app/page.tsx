5cd2b76ec6979a1b684e5704a6b2240f100d06b4
yu5 <fukanoyugo+dev@gmail.com>
2023-05-06 23:02:55 +0900

    Profile cardを追加した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import ProfileCard from '@/components/ProfileCard';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="">
+      <ProfileCard />
       <h1 className="text-center text-2xl">
         Your Content in the Cente
         これはダミーのタイトルです。ああああああああああああああああああああああああああああああああああああああ
