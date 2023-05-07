8b51e2c87b16c5f4a1d5b2adbea3c74c118766cb
yu5 <fukanoyugo+dev@gmail.com>
2023-05-07 23:10:27 +0900

    マージンを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,11 @@ import Navigation from '@/components/Navigtion';
 export default function Home() {
   return (
     <div className="">
-      <div className="mb-12">
+      <div className="mb-8">
         <ProfileCard />
       </div>
       <Navigation />
-      <h1 className="text-center text-2xl">
-        Your Content in the Cente
-        これはダミーのタイトルです。ああああああああああああああああああああああああああああああああああああああ
-        r
-      </h1>
+      <h1 className="text-2xl mt-8">これはダミーのコンテンツ</h1>
     </div>
   );
 }
