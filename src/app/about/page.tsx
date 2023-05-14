68613d81a4d6ef84e8b6053d0872cd34a9f8b1a5
yu5 <fukanoyugo+dev@gmail.com>
2023-05-14 23:24:57 +0900

    jsonで管理する

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,25 +1,34 @@
-import ProfileCard from '@/components/ProfileCard';
-import Navigation from '@/components/Navigtion';
 import MainLayout from '@/layouts/MainLayout';
+import fs from 'fs';
+import path from 'path';
 
 export default function Home() {
+  const dataPath = path.join(process.cwd(), '/src/app/about/data.json');
+  const rawData = fs.readFileSync(dataPath, 'utf8');
+  const data = JSON.parse(rawData);
   return (
     <MainLayout>
       <h2 className="text-2xl mb-4">Work</h2>
-      <div className="border-slate-800 border-2 rounded-md p-4">
-        <div className="flex justify-between">
-          <p>GMOペパボ株式会社</p>
-          <p className="text-slate-500">2022.4 - Present</p>
+      {data.work.map((work, index) => (
+        <div key={index} className="border-slate-800 border-2 rounded-md p-4">
+          <div className="flex justify-between">
+            <p>{work.conpany_name}</p>
+            <p className="text-slate-500">
+              {work.period_start} - {work.period_end}
+            </p>
+          </div>
+          <p>{work.position}</p>
         </div>
-        <p>SUZURI / エンジニア</p>
-      </div>
+      ))}
       <h2 className="text-2xl mt-8 mb-4">Skill</h2>
-      <div className="border-slate-800 border-2 rounded-md p-4">
-        <div className="flex justify-between">
-          <p>上級VR技術者</p>
-          <p className="text-slate-500">2022.1</p>
+      {data.skill.map((skill, index) => (
+        <div key={index} className="border-slate-800 border-2 rounded-md p-4">
+          <div className="flex justify-between">
+            <p>{skill.name}</p>
+            <p className="text-slate-500">{skill.date}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </MainLayout>
   );
 }
