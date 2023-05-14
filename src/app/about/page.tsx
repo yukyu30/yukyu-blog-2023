36d194f518f4e0f7dbd8a1e40b617c9552cf63ac
yu5 <fukanoyugo+dev@gmail.com>
2023-05-14 23:29:33 +0900

    方を書いた

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,26 +9,38 @@ export default function Home() {
   return (
     <MainLayout>
       <h2 className="text-2xl mb-4">Work</h2>
-      {data.work.map((work, index) => (
-        <div key={index} className="border-slate-800 border-2 rounded-md p-4">
-          <div className="flex justify-between">
-            <p>{work.conpany_name}</p>
-            <p className="text-slate-500">
-              {work.period_start} - {work.period_end}
-            </p>
+      {data.work.map(
+        (
+          work: {
+            conpany_name: string;
+            period_start: string;
+            period_end: string;
+            position: string;
+          },
+          index: number
+        ) => (
+          <div key={index} className="border-slate-800 border-2 rounded-md p-4">
+            <div className="flex justify-between">
+              <p>{work.conpany_name}</p>
+              <p className="text-slate-500">
+                {work.period_start} - {work.period_end}
+              </p>
+            </div>
+            <p>{work.position}</p>
           </div>
-          <p>{work.position}</p>
-        </div>
-      ))}
+        )
+      )}
       <h2 className="text-2xl mt-8 mb-4">Skill</h2>
-      {data.skill.map((skill, index) => (
-        <div key={index} className="border-slate-800 border-2 rounded-md p-4">
-          <div className="flex justify-between">
-            <p>{skill.name}</p>
-            <p className="text-slate-500">{skill.date}</p>
+      {data.skill.map(
+        (skill: { name: string; date: string }, index: number) => (
+          <div key={index} className="border-slate-800 border-2 rounded-md p-4">
+            <div className="flex justify-between">
+              <p>{skill.name}</p>
+              <p className="text-slate-500">{skill.date}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </MainLayout>
   );
 }
